2ca420654a1b0d79241c975f40fd4653f1fa7866
Lamber <420862234@qq.com>
2023-03-14 13:42:39 +0800

    deploy

---
+++ precache-manifest.07a60849fe0311ea79eb9411dcebb8d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aaceca7d0869b415664",
+    "url": "/morney-1-website/css/app.64d0f5fc.css"
+  },
+  {
+    "revision": "ee339c6ef238e84d7981877901f39855",
+    "url": "/morney-1-website/index.html"
+  },
+  {
+    "revision": "9aaceca7d0869b415664",
+    "url": "/morney-1-website/js/app.eafe6175.js"
+  },
+  {
+    "revision": "63a56a57990b689eafd7",
+    "url": "/morney-1-website/js/chunk-vendors.282fde38.js"
+  },
+  {
+    "revision": "b0c72820654d086f2b526db8c6a20387",
+    "url": "/morney-1-website/manifest.json"
+  },
+  {
+    "revision": "bf0993cb256eb824af1b0f4e77b924f7",
+    "url": "/morney-1-website/qrcode.png"
+  },
+  {
+    "revision": "45479365116572c4a1d9be128e884a18",
+    "url": "/morney-1-website/qrcode22.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-1-website/robots.txt"
+  }
+]);
